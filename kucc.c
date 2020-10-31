ade8e66cf0bc1dedf2154086664d3cb393cb4e8e
hota1024 <hotalog@hota1024.com>
2020-10-31 17:11:58 +0900

    🏷️ ASTを元にスタック操作命令を生成する関数を実装

--- kucc.c
+++ kucc.c
@@ -255,6 +255,45 @@ Node *primary()
   return new_node_num(expect_number());
 }
 
+// 抽象構文木を元にスタック操作命令を生成する。
+void gen(Node *node)
+{
+  if (node->kind == ND_NUM)
+  {
+    printf("  push %d\n", node->val);
+
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind)
+  {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+
+    case ND_MUL:
+      printf("  imul rax, rdi\n");
+      break;
+
+    case ND_DIV:
+      printf("  cqo\n");
+      printf("  idiv rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv)
 {
   if (argc != 2)
