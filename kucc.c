0c3a7611d0723ee1fd88429260bd878a3f414866
hota1024 <hotalog@hota1024.com>
2020-10-31 17:18:56 +0900

    🏷️ 四則演算のできる言語を実装🎉

--- kucc.c
+++ kucc.c
@@ -154,7 +154,14 @@ Token *tokenize(char *p)
       continue;
     }
 
-    if (*p == '+' || *p == '-')
+    if (
+      *p == '+' ||
+      *p == '-' ||
+      *p == '*' ||
+      *p == '/' ||
+      *p == '(' ||
+      *p == ')'
+    )
     {
       cur = new_token(TK_RESERVED, cur, p++);
 
@@ -302,35 +309,22 @@ int main(int argc, char **argv)
     return 1;
   }
 
+  // トークナイズしてパースする
   user_input = argv[1];
-
-  // トークナイズする
   token = tokenize(user_input);
+  Node *node = expr();
+
 
   // アセンブリの前半部分を出力
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  // 式の最初は数でなければならないので、それをチェックして
-  // 最初のmov命令を出力
-  printf("  mov rax, %d\n", expect_number());
+  gen(node);
 
-  // `+ <数>` あるいは `- <数>` というトークンの並びを消費しつつ
-  // アセンブリを出力
-  while (!at_eof())
-  {
-    if (consume('+'))
-    {
-      printf("  add rax, %d\n", expect_number());
-
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // スタックトップに式全体の値が残っているはずなので
+  // それをRAXにロードして関数からの返り値とする
+  printf("  pop rax\n");
   printf("  ret\n");
 
   return 0;
